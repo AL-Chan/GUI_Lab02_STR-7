6f6502d3d9dd9540ecb70d585fdc61e5c0da4854
Maxim Chan <90909658+AL-Chan@users.noreply.github.com>
2022-01-24 16:44:55 +0800

    Add files via upload
    Добавлены автотесты программы, программа доведена до ума (логика окончательно ушла в отдельный класс; добавлено отдельное поле для удаления спецсимволов и пробела из исходного предложения)

--- Lab2PartSTRVar7/Lab2PartSTRVar7/Form1.cs
+++ Lab2PartSTRVar7/Lab2PartSTRVar7/Form1.cs
@@ -25,38 +25,42 @@ namespace Lab2PartSTRVar7
 			Properties.Settings.Default.YourText = sentence;
 			Properties.Settings.Default.Save(); //сохранение + восстановление
 
-			//удаление символов в квадратных скобках,
-			//тем самым получается предложение без пробелов
-			sentence = System.Text.RegularExpressions.Regex.Replace
-				(sentence, "[.,:;!?№()&^#@1234567890 -]", String.Empty);
-			//во второе поле выводится предложение без пробелов
-			YourTextNoSpaces.Text = sentence;
 			//новый экземпляр класса Логика
-			var Reserv = new Logic();
-			Reserv.Dub(sentence);
+			var Logic = new Logic();
 			//счетчик высчитывает кол-во повторов
-			int dub = Reserv.Dub(sentence);
-			//в третье выведется вывод о кол-ве повторов
-			Reserv.ProcessGoing(dub);
-			var Itog = Reserv.ProcessGoing(dub);
+			int dub = Logic.Dub(sentence);
+			//во второе поле выводится предложение без пробелов
+			YourTextNoSpaces.Text = Logic.NewSentence(sentence);
+			//вывод сообщения о повторах
+			var Itog = Logic.ProcessGoing(dub);
+			//в третье поле выведется вывод о кол-ве повторов
 			ItogText.Text = Itog;
 		}
 
 		public class Logic
 		{
+			public string NewSentence (string sentence) //удаление символов, перечисленных в квадратных скобках
+			{
+			sentence = System.Text.RegularExpressions.Regex.Replace
+				(sentence, "[.,:;!?№()&^#@1234567890$%*+=_ -]", String.Empty);
+				return sentence;
+			}
+
 			public int Dub(string sentence)
             {
+				var Logic = new Logic();
 				var dub = 0;
-
+				sentence = Logic.NewSentence(sentence);
 				for (int a = 0; a < sentence.Length - 1; a++)
 					if (sentence[a] == sentence[a + 1])
 					{
 						dub++; //счётчик с шагом 1
 					}
+
 				return dub;
 			}
 
-			public string ProcessGoing(int dub)
+			public string ProcessGoing(int dub) //для выводного сообщения
 			{
 				string Itog = "";
 				switch(dub)
@@ -68,14 +72,12 @@ namespace Lab2PartSTRVar7
 						Itog = ("Количество повторяющихся пар букв в предложении: " + dub);
 						break;
 				}
-
 				return Itog;
 			}
-
 		}
 
-        private void abouttask_Click(object sender, EventArgs e)
-        {
+        private void abouttask_Click(object sender, EventArgs e) // кнопка Посмотреть задание
+		{
 			MessageBox.Show("Дано предложение. Определить, сколько в нем " +
 				"повторяющихся пар букв. Пробелы не учитывать.",
 				"Задание на строки, вариант 7");
---
+++ Lab2PartSTRVar7/Lab2PartSTRVar7Tests/LogicTests.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using static Lab2PartSTRVar7.Form1;
+
+namespace Lab2PartSTRVar7.Tests
+{
+    [TestClass()]
+    public class LogicTests
+    {
+
+        [TestMethod()]
+        public void OdinPovtor()
+        {
+            string sentence = "Мама!мыла @#аквариу!м";
+            Logic Logic= new Logic();
+            int dub = Logic.Dub(sentence);
+            var Itog = Logic.ProcessGoing(dub);
+             Assert.AreEqual("Количество повторяющихся пар букв в предложении: 1", Itog);
+        }
+
+        [TestMethod()]
+        public void TriPovtora()
+        {
+            string sentence = "зооорганизация %:ялюблюживотных";
+            Logic Logic = new Logic();
+            int dub = Logic.Dub(sentence);
+            var Itog = Logic.ProcessGoing(dub);
+            Assert.AreEqual("Количество повторяющихся пар букв в предложении: 3", Itog);
+        }
+
+        [TestMethod()]
+        public void NetPovtora()
+        {
+            string sentence = "Воин Цусимы";
+            Logic Logic = new Logic();
+            int dub = Logic.Dub(sentence);
+            var Itog = Logic.ProcessGoing(dub);
+            Assert.AreEqual("В этом предложении нет повторяющихся пар букв", Itog);
+        }
+
+    }
+}
+
